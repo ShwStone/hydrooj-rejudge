5434a860b11ffff4a81eeeb7cd8d56fcdf0e0933
ShwStone <HaowenShi@outlook.com>
2023-06-27 14:40:30 +0800

    Auto commit from Arch Linux.

--- scripts/problem.ts
+++ scripts/problem.ts
@@ -1,11 +1,12 @@
-import { Schema, RecordModel } from 'hydrooj';
+import { Schema, RecordModel, problem } from 'hydrooj';
+import { ObjectId } from 'mongodb';
 
 export async function run({pid, contest}, report: Function) {
 	let filter = Schema.object();
 	
-	if (pid && contest) filter = Schema.object({ pid: pid, contest: contest });
+	if (pid && contest) filter = Schema.object({ pid: pid, contest: ObjectId(contest) });
 	else if (pid) filter = Schema.object({ pid: pid });
-	else if (contest) filter = Schema.object({ contest: contest });
+	else if (contest) filter = Schema.object({ contest: ObjectId(contest) });
 	else filter = Schema.object();
 
 	const records = await RecordModel.coll.find(filter, { _id: true, domainId: true, pid: true });
@@ -18,9 +19,11 @@ export async function run({pid, contest}, report: Function) {
 				report({ message: e });
 				continue;
 			}
+			
+			const isContest = record.contest && record.contest.toString() !== '000000000000000000000000';
 
 			try {
-				await RecordModel.judge(record.domainId, record._id);
+				await RecordModel.judge(record.domainId, record._id, -20, isContest ? { detail: false } : {});
 			} catch(e) {
 				report({ message: e });
 				continue;
