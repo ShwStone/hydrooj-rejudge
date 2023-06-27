0ac427fdcdb9e473387c6a52f38b2b0692b8627a
ShwStone <HaowenShi@outlook.com>
2023-06-27 13:58:18 +0800

    catch the errors

--- scripts/problem.ts
+++ scripts/problem.ts
@@ -11,9 +11,21 @@ export async function run({pid, contest}, report: Function) {
 	const records = await RecordModel.coll.find(filter, { _id: true, domainId: true, pid: true });
 
 	for await (const record of records) {
-		if (record.pid != -1 && record.pid != 1) {
-			await RecordModel.reset(record.domainId, record._id, true);
-			await RecordModel.judge(record.domainId, record._id);
+		if (record.pid != -1) {
+			try {
+				await RecordModel.reset(record.domainId, record._id, true);
+			} catch(e) {
+				report({ message: e });
+				continue;
+			}
+
+			try {
+				await RecordModel.judge(record.domainId, record._id);
+			} catch(e) {
+				report({ message: e });
+				continue;
+			}
+
 			report({ message: record._id.toString() + " finished."});
 		}
 	}
