936e978357d8120618ecbaf82bbe7e6a81f13289
ShwStone <HaowenShi@outlook.com>
2023-06-27 10:58:09 +0800

    problem

--- index.ts
+++ index.ts
@@ -5,21 +5,14 @@ import { definePlugin, Context, Schema } from 'hydrooj';
 export default definePlugin({
 	apply(ctx: Context) {
 		ctx.addScript(
-			'rejudgeContest', 'rejudge for a certain contest',
-			Schema.object({
-				cid: Schema.string().required()
-			}),
-			(...args) => require('./scripts/contest').run(...args)
-		);
-		ctx.addScript(
 			'rejudgeProblem', 'rejudge for a certain problem',
 			Schema.object({
-				pid: Schema.number().required()
+				pid: Schema.number(),
+				contest: Schema.string()
 			}),
 			(...args) => require('./scripts/problem').run(...args)
 		);
 		ctx.i18n.load('zh', {
-			'rejudge for a certain contest': '重测一场比赛的所有提交',
 			'rejudge for a certain problem': '重测一道题目的所有提交'
 		});
 	}
--- scripts/problem.ts
+++ scripts/problem.ts
@@ -1,13 +1,20 @@
 import { RecordDoc, RecordModel } from 'hydrooj';
 
-export async function run({pid}, report: Function) {
+export async function run({pid, contest}, report: Function) {
+	let filter = {};
 	
-	const records = RecordModel.coll.find(pid == -111 ? {} : {pid: pid}, {_id: true,domainId: true, pid: true});
-	records.forEach((record) => {
-		if (record.pid != -1) {
-			report({message: record._id.toString()});
-			RecordModel.reset(record.domainId, record._id, true);
-			RecordModel.judge(record.domainId, record._id);
+	if (pid && contest) filter = { pid: pid, contest: contest };
+	else if (pid) filter = { pid: pid };
+	else if (contest) filter = { contest: contest };
+	else filter = {};
+
+	const records = await RecordModel.coll.find(filter, { _id: true, domainId: true, pid: true });
+
+	for await (const record of records) {
+		if (record.pid != -1 && record.pid != 1) {
+			await RecordModel.reset(record.domainId, record._id, true);
+			await RecordModel.judge(record.domainId, record._id);
+			report({ message: record._id.toString() + " finished."});
 		}
-	});
+	}
 }
